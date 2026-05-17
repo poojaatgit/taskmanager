7d51b3cf5b2caa40804651e765ac52f0214c464e
Pooja C <poojachekkachantakath@gmail.com>
2026-05-17 14:23:32 +0400

    Initial commit - Spring Boot task manager API

---
+++ src/main/java/com/example/demo/TaskmanagerApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("i am working on it");
+		SpringApplication.run(TaskmanagerApplication.class, args);
+	}
+
+}
